a54ca13c26b7bca6af918a6eefa5f3329c16bd4c
MungangaThelly <daddymulongo@yahoo.fr>
2025-03-26 11:53:46 +0100

    update

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,7 +5,6 @@ import Home from './pages/Home';
 import Header from './components/Header';
 import Footer from './components/Footer';
 //import Search from './components/Search';
-//import AuctionDetail from './pages/AuctionDetail';
 //import CreateAuction from './pages/CreateAuction';
 //import Login from './pages/Login';
 //import Register from './pages/Register';
