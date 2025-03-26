2bc3d57421673acf7d487e99a4450a941e908d90
MungangaThelly <daddymulongo@yahoo.fr>
2025-03-26 11:27:30 +0100

    update

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,7 +4,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import Header from './components/Header';
 import Footer from './components/Footer';
-import Search from './components/Search';
+//import Search from './components/Search';
 //import AuctionDetail from './pages/AuctionDetail';
 //import CreateAuction from './pages/CreateAuction';
 //import Login from './pages/Login';
@@ -17,7 +17,6 @@ function App() {
       <Header />
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/search/:query" element={<Search />} />
       </Routes>
       <Footer />
     </Router>
